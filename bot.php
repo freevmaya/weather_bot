ce926f53a8a31f102a7138778bbdb27db672a763
Vadim Frolov <@vmaya>
2026-01-11 12:11:45 +0500

    requlary

--- bot.php
+++ bot.php
@@ -1,4 +1,10 @@
 <?php
+
+// Включить все ошибки
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/src/Vmaya/engine.php';
 
